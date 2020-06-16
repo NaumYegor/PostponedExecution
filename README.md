4cf43a505fbd7b108e259b9e2e5c85c23b38e91b
naum <yehornaumenko@gmail.com>
2020-06-16 13:30:30 +0300

    Added a getter for config

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-<p>It is a service that makes transactions on specified date(year, month, day, hour, minute, second).</p>
+<p>It is a service that makes transactions on specified date.(year, month, day, hour, minute, second)</p>
+<p>All important and sensitive data is stored in a <b>config.yaml</b> file.</p>
 <p>Readme will be updated.</p>
--- main.go
+++ main.go
@@ -2,12 +2,38 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
 )
 
+var (
+	confPath string = "config.yaml"
+)
+
+type YamlConf struct {
+	Config struct {
+		Port uint16 `yaml:"port"`
+	}
+}
+
+func (conf *YamlConf) getConf() *YamlConf {
+	yamlFile, err := ioutil.ReadFile(confPath)
+	if err != nil {
+		log.Printf("config-yaml-file error \n%e", err)
+	}
+
+	err = yaml.Unmarshal(yamlFile, conf)
+	if err != nil {
+		log.Printf("error in unmarshaling config\n%e", err)
+	}
+
+	return conf
+}
+
 func main() {
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprintf(writer, "Hello, World!")
-	})
-	http.ListenAndServe(":5303", nil)
+	var conf YamlConf
+	conf.getConf()
+
+	fmt.Printf("The port is %d", conf.Config.Port)
 }
